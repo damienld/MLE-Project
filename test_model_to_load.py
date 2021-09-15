d5a1aae2c833391c1811823e6d28aa3413466771
damienld <dledirach@yahoo.fr>
2021-09-15 16:46:40 +0200

    Update test_model_to_load.py
    1 variable renamed

--- test_model_to_load.py
+++ test_model_to_load.py
@@ -1,5 +1,5 @@
 from model_to_load import ModelFromFiles
 
 def test_load():
-    list_models = ModelFromFiles.load_all_models_to_list()
-    assert len(list_models) == 4
+    lst_models = ModelFromFiles.load_all_models_to_list()
+    assert len(lst_models) == 4
